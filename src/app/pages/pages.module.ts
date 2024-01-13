62c7fd57dfffd8dedd185df2f0c0a66d8da58d37
Moisés <msportela2@hotmail.com>
2024-01-13 14:48:36 -0300

    criação inicial de pages

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Module Routing
+import {PagesRoutingModule} from "./pages.routing.module";
+//Pages
+
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+  ]
+})
+export class PagesModule { }
